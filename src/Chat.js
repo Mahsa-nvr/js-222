c73edbf8da782ac9d998ad6e10139c7af504d05c
Mahsa-nvr <46869829+Mahsa-nvr@users.noreply.github.com>
2019-03-24 01:01:56 +0430

    Add files via upload

--- src/Chat.js
+++ src/Chat.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Button } from 'reactstrap';
 
 class Chat extends  React.Component{
+
+    logoff = (props) => {
+        this.props.history.push('/logoff');
+    }
+    
  render() {
      return(
          <div className="b3">
              
              
-             <Button color="primary" size="SM" className="b4">Logoff</Button>
+             <Button onClick={this.logoff} color="primary" size="SM" className="b4">Logoff</Button>
              
          </div>
      );
