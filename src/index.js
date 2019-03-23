6bd01652be501c35cd97ec2625d7c592f8896b29
Mahsa-nvr <46869829+Mahsa-nvr@users.noreply.github.com>
2019-03-24 01:08:06 +0430

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 //import Login from './Login';
 //import Create from './Create';
 //import Logoff from './Logoff';
 
+import App from './App';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import Chat from './Chat';
+//import Chat from './Chat';
 
-ReactDOM.render(<Chat/>, document.getElementById('root'));
+
+
+ReactDOM.render(<Router><App/></Router>
+, document.getElementById('root'));
 
 
