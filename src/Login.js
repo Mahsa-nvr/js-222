21a1fabedb85ddcd69ea31c324f2081e64abde81
Mahsa-nvr <46869829+Mahsa-nvr@users.noreply.github.com>
2019-03-24 01:05:15 +0430

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,19 @@ import React from 'react';
 import {Container, Form, FormGroup, Label, Input ,Button} from 'reactstrap';
 
 class Login extends React.Component{
+
+
+
+
+  continue = (props) => {
+    this.props.history.push('/chat');
+  }
+
+  create = (props) => {
+    this.props.history.push('/create');
+  }
+
+
 render() {
     return (
         <Container>
@@ -20,8 +33,8 @@ render() {
           <Input type="password" name="password" id="Password" placeholder="Enter Password" />
         </FormGroup>
         </Form>
-        <Button color="primary" size="lg" block>Continue</Button>
-        <Button color="secondary" size="sm" className="b1">Create New Account</Button>
+        <Button onClick={this.continue} color="primary" size="lg" block>Continue</Button>
+        <Button onClick={this.create} color="secondary" size="sm" className="b1">Create New Account</Button>
 
         </Container>
     )
