5e576a3190728e135a25f925248bd43cf95abcc8
Mahsa-nvr <mahsaanvari2@gmail.com>
2019-04-06 18:33:29 +0430

    install react chat window dependency for chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,28 +1,64 @@
 import React from 'react';
-import { Button} from 'reactstrap';
-
-
+import { Container,Button} from 'reactstrap';
+import {Launcher} from 'react-chat-window'
 
 class Chat extends  React.Component{
+    constructor() {
+        super();
+        this.state = {
+          messageList: []
+          
+        };
+      }
+
 
     logoff = (props) => {
         this.props.history.push('/logoff');
-    }////
+    }
+
+    _onMessageWasSent(message) {
+        this.setState({
+          messageList: [...this.state.messageList, message]
+        })
+      }
+
+
+      _sendMessage(text) {
+        if (text.length > 0) {
+          this.setState({
+            messageList: [...this.state.messageList, {
+              author: 'them',
+              type: 'text',
+              data: { text }
+            }]
+          })
+        }
+      }
     
     
 
  render() {
      return(
         
-         <div className="b3">
+         <Container>
 
-             <Button className="b5" color="primary">start</Button>
-            <Button onClick={this.logoff} color="primary" size="SM" className="b4">Logoff</Button>
-            
-             <textarea placeholder="Message text" className="b7"></textarea>
-             <Button className="b8" color="primary" >send</Button>
-             
-         </div>
+
+<Launcher 
+        agentProfile={{
+          teamName: 'robot chat',
+          //imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+        }}
+        onMessageWasSent={this._onMessageWasSent.bind(this)}
+        messageList={this.state.messageList}
+        showEmoji
+    
+      />
+
+
+             <Button className="b5" color="primary" block>Start</Button>
+            <Button onClick={this.logoff} color="primary"  className="b4" block>Logoff</Button>
+
+         </Container>
          
      );
  }
