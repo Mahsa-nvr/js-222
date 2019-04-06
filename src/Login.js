dd4c9305a2779bcf79906a504172aa9614589fe8
Mahsa-nvr <mahsaanvari2@gmail.com>
2019-04-06 16:10:21 +0430

    state of login form

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import {Container, Form, FormGroup, Label, Input ,Button} from 'reactstrap';
+import {HandleChange} from "./Utility";
 
 class Login extends React.Component{
-
+  constructor(props){
+    super(props);
+      this.state = {
+        username : '',
+        password : ''
+      
+    }
+  }
 
 
 
   continue = (props) => {
     this.props.history.push('/chat');
-  }
+    //console.log(this.state.username);
+  };
 
   create = (props) => {
     this.props.history.push('/create');
+    //console.log(this.state.username);
   }
 
 
 render() {
     return (
+     
+      
         <Container>
         
             <h1>
@@ -25,18 +37,21 @@ render() {
             
             <Form>
         <FormGroup>
+        
           <Label for="Username">Username :</Label>
-          <Input type="text" name="username" id="Username" placeholder="Enter Username" />
+          <Input type="text" name="username" id="Username" placeholder="Enter Username"  onChange={(e)=> HandleChange.call(this, e) } />
+        
         </FormGroup>
         <FormGroup>
           <Label for="Password">Password :</Label>
-          <Input type="password" name="password" id="Password" placeholder="Enter Password" />
+    <Input type="password" name="password" id="Password" placeholder="Enter Password"  onChange={(e)=> HandleChange.call(this, e)} />
         </FormGroup>
         </Form>
         <Button onClick={this.continue} color="primary" size="lg" block>Continue</Button>
         <Button onClick={this.create} color="secondary" size="sm" className="b1">Create New Account</Button>
 
         </Container>
+       
     )
 }
 }
