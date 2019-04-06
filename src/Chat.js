e8ae36a97230c328d15ff0cd16e4d334d34113b6
Mahsa-nvr <mahsaanvari2@gmail.com>
2019-04-06 16:04:46 +0430

    first ui of chat form

--- src/Chat.js
+++ src/Chat.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button} from 'reactstrap';
+
+
 
 class Chat extends  React.Component{
 
     logoff = (props) => {
         this.props.history.push('/logoff');
-    }
+    }////
     
+    
+
  render() {
      return(
+        
          <div className="b3">
-             
-             
-             <Button onClick={this.logoff} color="primary" size="SM" className="b4">Logoff</Button>
+
+             <Button className="b5" color="primary">start</Button>
+            <Button onClick={this.logoff} color="primary" size="SM" className="b4">Logoff</Button>
+            
+             <textarea placeholder="Message text" className="b7"></textarea>
+             <Button className="b8" color="primary" >send</Button>
              
          </div>
+         
      );
  }
 }
-export default Chat;
